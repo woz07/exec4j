090183aa172a2c109b58562a3f34a84d1b2ca79f
woz07 <105782312+woz07@users.noreply.github.com>
2024-02-29 14:25:07 +0000

    Add: All files

---
+++ src/main/java/github/woz07/exec4j/Executor.java
@@ -0,0 +1,34 @@
+package github.woz07.exec4j;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author woz07
+ *
+ * Executor.java
+ * This is the class that must be initialized in order to execute programs
+ */
+
+public class Executor {
+    /**
+     *
+     * @param command The command you would like to execute
+     * @return Returns the end result code, 0 for success, >0 for issue
+     * @throws IOException Thrown because of builder.start()
+     * @throws InterruptedException Thrown because of process.waitFor()
+     */
+    public synchronized int execute(String command) throws IOException, InterruptedException {
+        if (command == null) return 1;
+        List<String> list = new ArrayList<>(Arrays.asList(command.split("\\s+")));
+        ProcessBuilder builder = new ProcessBuilder(list);
+        builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectError(ProcessBuilder.Redirect.INHERIT);
+        Process process = builder.start();
+
+        return process.waitFor();
+    }
+}
